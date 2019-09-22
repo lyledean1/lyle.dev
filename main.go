b48805651fabf8cf87e4a96c4e71eea28738e961
Lyle Dean <lylebd1@gmail.com>
2019-09-22 19:49:00 +0100

    remove port

--- main.go
+++ main.go
@@ -1,25 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
-	"os"
 )
 
-func getPort() string {
-	port := os.Getenv("PORT")
-	if port == "" {
-		return ":3000"
-	}
-	return ":" + port
-}
-
 func main () {
-	fmt.Println(getPort())
 	router := gin.Default()
 	router.Static("/css", "public/css")
 	router.Static("/images", "public/images")
 	router.Static("/js", "public/js")
 	router.StaticFile("/", "public/index.html")
-	router.Run(getPort())
+	router.Run()
 }
