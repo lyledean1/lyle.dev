08b5989f54e0f1133f8115916323c224ae456c38
Lyle Dean <lylebd1@gmail.com>
2019-09-22 19:56:41 +0100

    add ports back

--- main.go
+++ main.go
@@ -1,10 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"os"
 )
 
+func getPort() string {
+	port := os.Getenv("PORT")
+	if port == "" {
+		return ":3000"
+	}
+	return ":" + port
+}
+
 func main () {
+	fmt.Println(getPort())
 	router := gin.Default()
 	router.Static("/css", "public/css")
 	router.Static("/images", "public/images")
